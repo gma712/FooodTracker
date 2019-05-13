6663e1712d42f4ae96d6dff8e77c164a28c1ef7b
gma712 <yuhei_kajima@nuco.co.jp>
2019-05-13 21:15:29 +0900

    Initial Commit

---
+++ FooodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FooodTracker
+//
+//  Created by 梶間悠平 on 2019/05/13.
+//  Copyright © 2019 gma712. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
