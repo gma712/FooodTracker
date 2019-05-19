0523fd17f7689a18eb572acff1575b073268c880
gma712 <yuhei_kajima@nuco.co.jp>
2019-05-19 12:45:34 +0900

    add selectImageFromPhotoLibrary

---
+++ FooodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FooodTracker
+//
+//  Created by 梶間悠平 on 2019/05/19.
+//  Copyright © 2019 gma712. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
