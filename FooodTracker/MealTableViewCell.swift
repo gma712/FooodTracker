17a02cd77d2f3faade065139ade77458b7b01ce3
yuhei_kajima <yuhei_kajima@nuco.co.jp>
2019-05-25 18:16:21 +0900

    updated

---
+++ FooodTracker/MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MealTableViewCell.swift
+//  FooodTracker
+//
+//  Created by gma712 on 2019/05/25.
+//  Copyright © 2019 gma712. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
